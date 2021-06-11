225a9c26535415bcecd60c7d1d45346f54a774b3
Andrew High <andrewhigh4@hotmail.com>
2021-06-11 13:20:54 +0100

    cabinCrewMember tests passing

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewMemberTest {
+
+        private CabinCrewMember cabinCrewMember;
+
+        @Before
+        public void before(){
+            cabinCrewMember = new CabinCrewMember("Charles", "Flight Attendant");
+        }
+
+        @Test
+        public void cabinCrewMemberHasName(){
+            assertEquals("Charles", cabinCrewMember.getName());
+        }
+
+        @Test
+        public void cabinCrewMemberCanSetName(){
+            cabinCrewMember.setName("Anthony");
+            assertEquals("Anthony", cabinCrewMember.getName());
+        }
+
+        @Test
+        public void cabinCrewMemberHasRank(){
+            assertEquals("Flight Attendant", cabinCrewMember.getRank());
+        }
+
+        @Test
+        public void cabinCrewMemberCanSetRank(){
+            cabinCrewMember.setRank("Senior Flight Attendant");
+            assertEquals("Senior Flight Attendant", cabinCrewMember.getRank());
+        }
+
 }
+
